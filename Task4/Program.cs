a0b9203bfe009d14a4e8ee090b6a0e3a6d855f02
YuriiSvereda <aderevs@gmail.com>
2024-02-24 18:09:23 +0200

    working version of task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static int counter = 0;
+    static object block = new object();
+    static async Task FunctionAsync()
+    {
+        for (int i = 0; i < 50; ++i)
+        {
+            await Task.Run(() =>
+            {
+                // Встановлюється блокування кожен (50!) разів у новий object (boxing).
+                lock (block)
+                {
+                    // Виконання деякої роботи потоком ...
+                    Console.WriteLine(++counter);
+                }
+            });
+        }
+    }
+
+    static async Task Main()
+    {
+        Task[] tasks = { FunctionAsync(), FunctionAsync(), FunctionAsync() };
+
+        await Task.WhenAll(tasks);
+
+        // Delay
+        Console.ReadKey();
+    }
+}
