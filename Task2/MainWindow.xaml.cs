eade059d1737f0a5424fede48099675fd746dcef
YuriiSvereda <aderevs@gmail.com>
2024-02-24 17:34:25 +0200

    working version of task 2

--- Task2/MainWindow.xaml.cs
+++ Task2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Task2
 {
@@ -16,9 +18,46 @@ namespace Task2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        bool isConnected = false;
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += Timer_Tick;
+        }
+
+        private async void Timer_Tick(object sender, EventArgs e)
+        {
+            await Task.Delay(1000);
+            if (isConnected)
+                text.Text += "\nДані отримані";
+        }
+        private async void ConnectToDB(object sender, RoutedEventArgs e)
+        {
+            Loading("Підключення");
+            await Task.Delay(new Random().Next() % 2000 + 3000);
+            isConnected = true;
+            text.Text = "Підключено до бази даних";
+            timer.Start();
+        }
+        private async void DisconnectDB(object sender, RoutedEventArgs e)
+        {
+            Loading("Відключення");
+            isConnected = false;
+            await Task.Delay(new Random().Next() % 2000 + 3000);
+            text.Text = "Відключено від бази дани";
+            timer.Stop();
+        }
+        private async Task Loading(string messege)
+        {
+            text.Text = messege;
+            for (int i = 0; i < 3; i++)
+            {
+                await Task.Delay(1000);
+                text.Text += " .";
+            }
         }
     }
 }
